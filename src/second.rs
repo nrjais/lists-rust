1c04e2fd1f0c2c261f6f591cf93ce19526cfa707
Neeraj Jaiswal <neerajj85@gmail.com>
2019-03-26 09:36:07 +0530

    Made list generic and add peek method

--- src/second.rs
+++ src/second.rs
@@ -1,23 +1,23 @@
 use std::mem;
 
 #[allow(dead_code)]
-pub struct List {
-  head: Link,
+pub struct List<T> {
+  head: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-  elem: i32,
-  next: Link,
+struct Node<T> {
+  elem: T,
+  next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
   pub fn new() -> Self {
     List { head: None }
   }
 
-  pub fn push(&mut self, elem: i32) {
+  pub fn push(&mut self, elem: T) {
     let new_node = Box::new(Node {
       elem,
       next: self.head.take(),
@@ -26,15 +26,27 @@ impl List {
     self.head = Some(new_node);
   }
 
-  pub fn pop(&mut self) -> Option<i32> {
+  pub fn pop(&mut self) -> Option<T> {
     self.head.take().map(|n| {
       self.head = n.next;
       n.elem
     })
   }
+
+  pub fn peek(&self) -> Option<&T> {
+    self.head.as_ref().map(|node| {
+      &node.elem
+    })
+  }
+
+  pub fn peek_mut(&mut self) -> Option<&mut T> {
+    self.head.as_mut().map(|n| {
+      &mut n.elem
+    })
+  }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
   fn drop(&mut self) {
     let mut cur_link = self.head.take();
     while let Some(mut boxed_node) = cur_link {
@@ -93,6 +105,23 @@ mod tests {
     // Check exhaustion
     assert_eq!(list.pop(), Some(1));
     assert_eq!(list.pop(), None);
+  }
+
+  #[test]
+  fn peek() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+    list.peek_mut().map(|value| {
+      *value = 42
+    });
+
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
   }
 }
 
